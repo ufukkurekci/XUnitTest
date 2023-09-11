6f7fd58f5a6f08dba18ed0b9da7cee6e226c4d18
ufukkurekcii <ufukkurekci@hotmail.com>
2023-09-11 16:59:35 +0300

    Add project files.

---
+++ UnitTest.APP/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.APP
+{
+    public class Calculator
+    {
+
+        public int  Sum(int a , int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ XUnitTest.Test/CalTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.APP;
+using Xunit;
+
+namespace XUnitTest.Test
+{
+    public class CalTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            int a = 5;
+            int b = 6;
+            var cal = new Calculator();
+
+            var total = cal.Sum(a, b);
+
+            Assert.Equal(11, total);
+        }
+    }
+}
